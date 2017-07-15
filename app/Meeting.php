20a42f3c26337bfa06472cee8b48f016e8faa216
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-07-15 20:34:26 +0700

    relasi->many to many->meeting->user

--- app/Meeting.php
+++ app/Meeting.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meeting extends Model
 {
-    //
+    protected $fillable = ['time','title','description'];
+
+    //many to many
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
